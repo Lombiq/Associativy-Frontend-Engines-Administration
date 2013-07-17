8b4fb7eb577367cee7b46fd75728a7bca8f8f779
Piedone <piedone@pyrocenter.hu>
2013-07-17 22:12:43 +0200

    Changing LazyFields to internal

--- Models/Pages/Admin/AssociativyManageGraphFrontendsPart.cs
+++ Models/Pages/Admin/AssociativyManageGraphFrontendsPart.cs
@@ -8,11 +8,11 @@ namespace Associativy.Frontends.Administration.Models.Pages.Admin
     public class AssociativyManageGraphFrontendsPart : ContentPart
     {
         private readonly LazyField<IEnumerable<IEngineDescriptor>> _frontendEngines = new LazyField<IEnumerable<IEngineDescriptor>>();
-        public LazyField<IEnumerable<IEngineDescriptor>> FrontendEnginesField { get { return _frontendEngines; } }
+        internal LazyField<IEnumerable<IEngineDescriptor>> FrontendEnginesField { get { return _frontendEngines; } }
         public IEnumerable<IEngineDescriptor> FrontendEngines { get { return _frontendEngines.Value; } }
 
         private readonly LazyField<GraphSettings> _settings = new LazyField<GraphSettings>();
-        public LazyField<GraphSettings> SettingsField { get { return _settings; } }
+        internal LazyField<GraphSettings> SettingsField { get { return _settings; } }
         public GraphSettings GraphSettings
         {
             get { return _settings.Value; }
